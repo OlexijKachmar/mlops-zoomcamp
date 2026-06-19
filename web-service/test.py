4423ea0f08d37068b14c03e8fac078bd788c29b6
OlexijKachmar <43219687+OlexijKachmar@users.noreply.github.com>
2026-06-19 20:44:46 +0000

    Added web service

---
+++ web-service/test.py
@@ -0,0 +1,20 @@
+# import predict
+
+ride = {
+    "PULocationID": 10,
+    "DOLocationID": 50,
+    "trip_distance": 40  
+}
+
+# features = predict.prepare_features(ride)
+# prediction = predict.predict(features)
+# print(prediction)
+
+import requests
+
+url = "http://localhost:9696/predict"
+response = requests.post(
+    url, json=ride
+)
+
+print(response.json())
